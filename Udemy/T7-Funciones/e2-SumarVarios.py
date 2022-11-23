64c47a543ab9fd66ddc11a151026386c9c3d2be3
EmiroMunoz01 <emiro_2001@icloud.com>
2022-11-23 13:26:58 -0500

    Sumar con varios argumentos

---
+++ Udemy/T7-Funciones/e2-SumarVarios.py
@@ -0,0 +1,14 @@
+# funcion sumar que recibe varios argumentos numericos
+
+# con el *args estamos diciendole que estamos recibiendo una cantidad de parametros indefinidos
+
+def sumar_valores(*args):
+    resultado = 0
+    # iteramos cada elemento, podremos recorrer como una tupla, puede ser cualquier nombre en vez de args
+    for valor in args:
+        resultado += valor
+    return resultado
+
+
+# Llamaremos nuestra funcion
+print(sumar_valores(3, 5, 9, 1))
